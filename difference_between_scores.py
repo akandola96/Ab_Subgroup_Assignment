c4afb0e496eff5f36d352ec3d9fbab7317cebbdf
Anon9559 <zcbtask@ucl.ac.uk>
2019-04-21 17:40:01 +0100

    updated assignemnt checker

--- difference_between_scores.py
+++ difference_between_scores.py
@@ -19,6 +19,7 @@ def difference_in_scores2(in_file,blout_queries_file,organism):
             FN = 0 
             TN = 0 
             misc = 0
+            alt_x = my_dict[x] + 'S'
             
             for row in reader:
                 score1 = float(row[2])
@@ -27,29 +28,61 @@ def difference_in_scores2(in_file,blout_queries_file,organism):
                 prediction = str(row[1])   
                 actual = str(row[6])     
     
+                # Assign entry to confusion matrix variable
+                
+                # If Heavy 1 is prediction and IGHV1 is actual TP + 1
                 if phrases(x, prediction) == True and \
                 phrases(my_dict[x],actual) == True:
-                    TP +=1 
-                    conf_matrix_type = 'TP'
+                    TP+=1
+                    conf_matrix_type = 'TP' 
                     
+                # If Heavy 1 is prediction and IGHV1S1 is actual TP + 1
+                elif phrases(x, prediction) == True and \
+                alt_x in actual:
+                    TP+=1
+                    conf_matrix_type = 'TP'
+                 
+                    
+                 # If Heavy 1 not prediction and IGHV1 is actual FN + 1   
                 elif phrases(x, prediction) == False and \
                 phrases(my_dict[x],actual) == True:
-                    FN +=1
+                    FN+=1
                     conf_matrix_type = 'FN'
                     
+                # If Heavy 1 not prediction and IGHV1S1 is actual FN + 1
+                elif phrases(x, prediction) == False and \
+                alt_x in actual:
+                    FN+=1
+                    conf_matrix_type = 'FN'
+                
+        
+                # if Heavy 1 is prediction and IGHV1 is not actual FP + 1
                 elif phrases(x,prediction) == True and \
                 phrases(my_dict[x],actual) == False:
-                    FP +=1
+                    FP+=1
                     conf_matrix_type = 'FP'
                     
+                # If Heavy 1 is prediction and IGHV1S1 is not actual FP + 1    
+                elif phrases(x,prediction) == True and \
+                alt_x not in actual:
+                    FP+=1
+                    conf_matrix_type = 'FP'
+                    
+                    
+                # If Heavy 1 is not prediction and IGHV1 is not actual TN + 1
                 elif phrases(x,prediction) == False and \
                 phrases(my_dict[x],actual) == False:
-                    TN +=1
-                    conf_matrix_type = 'TN'
-                
-                else:
-                    misc +=1
+                    TN+=1
+                    conf_matrix_type = 'TN'    
+                # If Heavy 1 is not prediction and IGHV1S1 is not actual TN+1
+                elif phrases(x,prediction) == False and \
+                alt_x not in actual:
+                    TN+=1
+                    conf_matrix_type = 'TN'    
                     
+                # Should always be 0
+                else:
+                    misc +=1   
                     
                 if conf_matrix_type == 'TP':
                     diff = score1 - score2
@@ -61,7 +94,7 @@ def difference_in_scores2(in_file,blout_queries_file,organism):
                     diff = score1 - score2
                     incorrect_diff.append(diff) 
                     
-                    
+        # Calculations            
         incorrect_mu = sum(incorrect_diff)/len(incorrect_diff)
         incorrect_sigma = statistics.stdev(incorrect_diff)
         
@@ -71,14 +104,16 @@ def difference_in_scores2(in_file,blout_queries_file,organism):
         print(incorrect_mu,incorrect_sigma)
         print(correct_mu,correct_sigma)
         
-        #norm dist for correct
+        # Normal Distributions
+        
+        # Norm dist for correct
         x = np.linspace(correct_mu - 3*correct_sigma, correct_mu +3*correct_sigma,100)
         plt.plot(x,scipy.stats.norm.pdf(x,correct_mu,correct_sigma),linewidth=3.5, label = 'Correct',color = 'Red')
-        #norm dist for incorrect
+        # Norm dist for incorrect
         z = np.linspace(incorrect_mu - 3*incorrect_sigma, incorrect_mu + 3*incorrect_sigma,100)
         plt.plot(z,scipy.stats.norm.pdf(z,incorrect_mu,incorrect_sigma),linewidth = 3.5,label = 'Incorrect',color = 'Blue')
         
-        #formatting
+        # Formatting
         plt.xticks(fontsize = 24)
         plt.yticks(fontsize = 24)
         plt.show
